670ce47bf0eb89c4b860a46b0838a09762a71759
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-08-23 05:54:22 +0700

    refactor: select server host by NODE_ENV
    create conditional to choose host for production or development

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const init = async () => {
     const server = Hapi.server({
         port: process.env.PORT || 5000,
-        host: '0.0.0.0',
+        host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
         routes: {
             cors: {
                 origin: ['*']
