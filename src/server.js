484020533da03935a0837d82db34f051746f60cb
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-08-23 11:26:51 +0700

    feat: call book api hapi framework

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const Hapi = require('@hapi/hapi')
 const routes = require('./routes')
 
+// Books
+const books = require('./api/books')
+const BooksService = require('./services/inMemory/BooksService')
+const BooksValidator = require('./validator/books')
+
 const init = async () => {
+  const booksService = new BooksService()
+
   const server = Hapi.server({
     port: process.env.PORT || 5000,
     host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
@@ -12,7 +19,15 @@ const init = async () => {
     }
   })
 
-  server.route(routes)
+  await server.register([
+    {
+      plugin: books,
+      options: {
+        service: booksService,
+        validator: BooksValidator
+      }
+    }
+  ])
 
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
