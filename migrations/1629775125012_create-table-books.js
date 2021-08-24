341170215142c4d8aeb36dfcde60c56504d25c61
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-08-24 08:14:18 +0700

    feat: create table migration
    Add table books migration to database postgres.

---
+++ migrations/1629775125012_create-table-books.js
@@ -0,0 +1,54 @@
+/* eslint-disable camelcase */
+exports.up = pgm => {
+  pgm.createTable('books', {
+    id: {
+      type: 'VARCHAR',
+      primaryKey: true,
+      notNull: true
+    },
+    name: {
+      type: 'VARCHAR',
+      notNull: true
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    author: {
+      type: 'VARCHAR',
+      notNull: true
+    },
+    summary: {
+      type: 'VARCHAR',
+      notNull: true
+    },
+    publisher: {
+      type: 'VARCHAR',
+      notNull: true
+    },
+    pageCount: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    readPage: {
+      type: 'INTEGER',
+      notNull: true
+    },
+    reading: {
+      type: 'BOOL',
+      notNull: true
+    },
+    created_at: {
+      type: 'VARCHAR',
+      notNull: true
+    },
+    updated_at: {
+      type: 'VARCHAR',
+      notNull: true
+    }
+  })
+}
+
+exports.down = pgm => {
+  pgm.dropTable('books')
+}
